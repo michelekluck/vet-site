9e036910444864b928b1bbb2053130df1ed710ea
michele nk <michelenamur27@gmail.com>
2026-05-27 16:53:11 -0300

    ServicesSection

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -3,17 +3,26 @@ import Button from "./Button"
 function ServicesSection() {
     return (
         <section>
-            <h2 className="font-bold">Serviços fornecidos</h2>
-            <div className='bg-white'>Consulta clínica especializada</div>
-            <div>Check-up preventivo</div>
-            <div className='bg-white'>Exames laboratoriais</div>
-            <div>Radiografia e ultrassom</div>
-            <p className="font-bold">Não encontrou o que estava procurando?</p>
-            <p>Entre em contato para consultar a possibilidade de atendimento!</p>
-            <Button>
-                <p>Consultar disponibilidade de procedimentos</p>
-            </Button>
-            <div>
+            <div className="">
+                <h2 className="font-bold text-[16px] p-4">Serviços fornecidos</h2>
+                <div className='bg-white text-center py-2'>Consulta clínica especializada</div>
+                <div className="py-2 text-center">Check-up preventivo</div>
+                <div className='bg-white py-2 text-center'>Exames laboratoriais</div>
+                <div className="py-2 text-center">Radiografia e ultrassom</div>
+                <div className='bg-white py-2 text-center'>Exames laboratoriais</div>
+                <div className="py-2 text-center">Radiografia e ultrassom</div>
+                <div className='bg-white py-2 text-center'>Exames laboratoriais</div>
+                <div className="py-2 text-center">Radiografia e ultrassom</div>
+                <div className='bg-white py-2 text-center'>Exames laboratoriais</div>
+                <div className="py-2 text-center">Radiografia e ultrassom</div>
+                <div className="p-4">
+                    <p className="font-bold py-2 text-center text-green text-[16px]">Não encontrou o que estava procurando?</p>
+                    <p className="mb-4">Entre em contato para consultar a possibilidade de atendimento!</p>
+                    <Button>
+                        <p className="w-[215px]">Consultar disponibilidade de procedimentos</p>
+                    </Button>
+                </div>
+
             </div>
         </section>
     )

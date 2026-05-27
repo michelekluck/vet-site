f91809f95bbf9a0c87897c679db4af3fdb61972f
michele nk <michelenamur27@gmail.com>
2026-05-27 15:31:31 -0300

    hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
+import whatsapp from "../assets/whatsapp.svg"
+
 function Button({ children, className = "", ...props }) {
     return (
-        <button className={`rounded-3xl px-5 py-1 bg-l-green text-black font-bold ${className}`}{...props}>
-            {children}
+        <button className={`rounded-full bg-l-green text-d-green font-bold relative flex items-center w-[340px] h-[52px] py-4 ${className}`}{...props}>
+            <img src={whatsapp} alt="Icone Whatsapp" className="ml-8"></img>
+            <p className="absolute left-1/2 -translate-x-1/2 text-center text-[15px]">{children}</p>
         </button>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,29 @@
 import Button from "./Button"
+import local from "../assets/local.svg"
+import iguana from "../assets/iguana.png"
 
 function Hero() {
     return (
         <section className="text-center">
-            <div className='bg-d-green text-white'>
-                <h1 className='font-bold'>Atendimento de animais silvestres e pets não convencionais à domicílio</h1>
-                <div className="border border-l-green rounded-3xl p-4">Em curitiba e Reigão Metropolitana</div>
-                <div>imagem</div>
+            <div className='text-white p-4 bg-d-green'>
+                <div>
+                    <h1 className='font-bold text-[20px]'>Atendimento de <br />
+                        <span className="bg-l-green text-d-green rounded-full pl-4 pr-1"> animais silvestres</span> e <br />
+                        <span className="bg-l-green text-d-green rounded-full pl-4 pr-1">pets não convencionais</span> à <br /> domicílio </h1>
+                    <div className="border border-l-green rounded-full text-[14px] font-bold m-4 relative flex items-center p-3 py-4">
+                        <img src={local} alt="Icone Local" className="ml-2"></img>
+                        <p className="absolute left-1/2 -translate-x-1/2 text-center">Em curitiba e Região <br /> Metropolitana</p>
+                    </div>
+                </div>
+                <div className="relative p-0">
+                    <img src={iguana} alt="Iguana" className="-ml-20 -mb-7"></img>
+                </div>
             </div>
-            <div className="font-bold text-black">
-                <p>Entre em contato!</p>
+            <div className="font-bold text-black mt-5 items-center p-4">
+                <p className="mb-4 text-[16px]">Entre em contato !</p>
                 <Button>
-                    <p>Whatsapp</p>
-                    <p>(41) 99646-2347</p>
+                    Whatsapp <br />
+                    (41) 99646-2347
                 </Button>
             </div>
         </section>

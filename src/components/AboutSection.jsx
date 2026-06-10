c4528498aa455d6fa0a90369ded46cd3e80f1392
michele nk <michelenamur27@gmail.com>
2026-06-10 16:46:24 -0300

    about section mobile

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,16 +3,16 @@ import image from "../assets/about.png"
 
 function AboutSection() {
     return (
-        <section className="bg-d-green">
+        <section className="bg-d-green mt-4">
             <div className="text-white p-4">
-                <h2 className="font-bold text-center">Quem sou eu? </h2>
-                <img src={image} alt="Imagem Samuel Kluck" className="mx-auto"></img>
-                <p className="text-[15px] mb-4">Nam sed velit nec urna fermentum convallis. Proin turpis dolor, ullamcorper non ante ut, tincidunt ultrices neque. Vivamus congue cursus tortor, vel consectetur odio blandit et. Vivamus ac imperdiet sem. Donec tempus tortor et justo ultrices, id elementum ante placerat. Pellentesque id lectus feugiat, euismod quam quis, vestibulum neque. Duis vel</p>
-                <Button>
-                    <p>Whatsapp</p>
-                </Button>
+                <h2 className="font-bold mt-4 mb-4">Quem sou eu? </h2>
+                <img src={image} alt="Imagem Samuel Kluck" className="mx-auto mb-4 mr-4"></img>
+                <div className="text-[14px]">
+                    <p>Sou Samuel Kluck, Médico Veterinário, com atuação voltada ao atendimento domiciliar de animais silvestres e pets não convencionais em Curitiba e região metropolitana.
+                        Possuo experiência prática no Centro de Apoio à Fauna Silvestre de Curitiba (CAFS), com atuação em clínica, manejo e acompanhamento de diferentes espécies. Atualmente, sou aprimorando em Clínica e Cirurgia de Pets Não Convencionais da Universidade Tuiuti do Paraná e médico veterinário do Vida Livre, o que me permite oferecer um atendimento técnico, atualizado e focado no bem-estar animal.</p>
+                </div>
             </div>
-        </section>
+        </section >
     )
 }
 

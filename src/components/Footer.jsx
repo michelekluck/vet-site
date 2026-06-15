62a384d5dfda85a7de77b901b54c706f8d174a67
michele nk <michelenamur27@gmail.com>
2026-06-15 14:10:49 -0300

    footer mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,37 @@
-import Button from "./Button"
+import Linkedin from "../assets/linkedin.svg?react"
+import Instagram from "../assets/instagram.svg?react"
 
 function Footer() {
     return (
         <footer>
-            <div className="bg-green">
-                <p className="text-white">Entre em contato!</p>
-                <Button>
-                    <p>(41) 99646-2347</p>
-                </Button>
-                <p className="text-white">Atendimento em Curitiba e Região Metropolitana</p>
-            </div>
-            <div className="bg-white">
-                <div className="flex items-center justify-between p-4">
-                    <div>Logo</div>
-                    <div>
-                        <span className='font-bold' >CRMV: 00000</span>
+            <div className="bg-green text-white mt-4 p-6">
+                <div>
+                    <p className="font-bold text-[16px] mb-4">Entre em contato:</p>
+                    <div className="text-[14px] leading-6">
+                        <p className="font-bold">Agendamento na Universidade Tuiuti</p>
+                        <a className="inline-block mb-2" href="tel:+5541333117955">(41) 3331-17955</a>
+                        <p className="font-bold">Agendamento à domicílio</p>
+                        <a href="tel:+5541333117955">(41) 99646-2347</a>
                     </div>
                 </div>
+                <div className="flex items-center gap-3 mt-[24px] mb-[24px]">
+                    <p className="font-bold text-[14px]">Me siga nas redes sociais</p>
+                    <div className="ml-auto flex gap-6">
+                        <Instagram className="text-white" />
+                        <Linkedin />
+                    </div>
+
+                </div>
+                <div className="bg-white h-[1px] w-auto mb-[24px]"></div>
+                <div className="text-[14px] ">
+                    <p>© 2026</p>
+                    <p>Todos os direitos reservados</p>
+                </div>
+                <div>
+                </div>
+
             </div>
-        </footer>
+        </footer >
     )
 }
 
--- src/components/IgSection.jsx
+++ src/components/IgSection.jsx
@@ -4,7 +4,7 @@ function IgSection() {
     return (
         <section>
             <div className="p-4 text-center">
-                <h2 className="font-bold">Quer saber mais sobre mim e meu trabalho?</h2>
+                <h2 className="text-d-green">Quer saber mais sobre mim e meu trabalho?</h2>
                 <p>Me siga no instagram!</p>
             </div>
             <div className="bg-l-green text-center items-center">
